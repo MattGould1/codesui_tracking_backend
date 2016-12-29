918b385f278f2d6b26d278cdc012d383be568640
Matt <matthew.gould@dentaldepartures.com>
2016-12-29 10:17:21 +0000

    factory for session

--- database/factory.js
+++ database/factory.js
@@ -12,7 +12,6 @@
 */
 
 const Factory = use('Factory')
-
 /*
 |--------------------------------------------------------------------------
 | User Model Blueprint
@@ -26,5 +25,61 @@ Factory.blueprint('App/Model/User', (fake) => {
     username: fake.username(),
     email: fake.email(),
     password: fake.password()
+  }
+})
+
+Factory.blueprint('App/Model/Session', (fake) => {
+
+  var iso_weeks = [
+    '2016 | 21',
+    '2016 | 22',
+    '2016 | 23',
+    '2016 | 24',
+    '2016 | 25',
+  ];
+
+  var utm_source = [
+    'google',
+    'facebook',
+    '',
+    'direct'
+  ];
+
+  var utm_name = [
+    'awesome',
+    'vuejs'
+  ];
+
+  var utm_term = [
+    'javascript',
+    'php'
+  ];
+
+  var utm_host_name = [
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.github.com',
+    'www.gitlab.com', //just coz
+  ];
+
+  var utm_content = [
+    'website',
+    'email'
+  ];
+  return {
+    id: fake.string(10),
+    utm_source: fake.pickone(utm_source),
+    utm_name: fake.pickone(utm_name),
+    utm_term: fake.pickone(utm_term),
+    utm_content: fake.string(20),
+    utm_host_name: fake.pickone(utm_host_name),
+    iso_week: fake.pickone(iso_weeks)
   }
 })
