cb57a76f6b15e999c17fc01acbfa927f4abbd6bb
Matt <matthew.gould@dentaldepartures.com>
2016-12-29 09:43:31 +0000

    update session controller

--- app/Http/Controllers/SessionController.js
+++ app/Http/Controllers/SessionController.js
@@ -7,7 +7,24 @@ class SessionController {
   }
 
   * create(request, response) {
+    //creates a session - this saves where a user comes from
+    var session = {};
+
+    session.id = response.session_id;
+
     //
+    if (response.utm_medium != '') {
+      session.utm_source = "{{none}}";
+      session.utm_medium = "{{direct}}";
+    } else {
+      session.utm_source = response.utm_source;
+      session.utm_medium = response.utm_medium;
+    }
+
+   if (response.utm_content != '') {
+     sesson.utm_content = response.utm_content;
+   }
+
   }
 
   * store(request, response) {
