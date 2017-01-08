2236d1a76819f9c8d26fac9259806679db23fdb6
Matt <matthew.gould@dentaldepartures.com>
2017-01-08 09:47:31 +0000

    create action

--- app/Http/Controllers/ActivityController.js
+++ app/Http/Controllers/ActivityController.js
@@ -2,6 +2,14 @@
 
 class ActivityController {
 
+  /*
+    Create a user, update the activity_id and 
+  */
+  * create (request, response) {
+    console.log(request.all());
+    response.send(true);
+  }
+
 }
 
 module.exports = ActivityController
