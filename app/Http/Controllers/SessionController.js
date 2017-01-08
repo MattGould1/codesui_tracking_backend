8c6baa8c6f626e4e0587f9313f52cbe2fa7ccc96
Matt <matthew.gould@dentaldepartures.com>
2017-01-08 05:18:07 +0000

    create initiate route for sessions + add code comment for request object

--- app/Http/Controllers/SessionController.js
+++ app/Http/Controllers/SessionController.js
@@ -1,9 +1,48 @@
 'use strict'
 
+const Session = use('App/Model/Session')
+const moment = use('moment')
+
 class SessionController {
 
   * index(request, response) {
     //
+  }
+  /*
+    Creates an initial session.
+
+    Request.all() = {
+      session_id,
+      utmTags {
+        source,
+        medium,
+        campaign,
+        term,
+        content
+      },
+      domain
+    }
+  */
+  * initiate(request, response) {
+    const body = request.all();
+
+    const iso_week = moment().year() + ' | ' + moment().isoWeek();
+
+    const session = yield Session.findOrCreate({
+      session_id: body.session_id
+    },
+      {
+      session_id: body.session_id,
+      utm_source: body.utmTags.source,
+      utm_medium: body.utmTags.medium,
+      utm_name: body.utmTags.campaign,
+      utm_term: body.utmTags.term,
+      utm_content: body.utmTags.content,
+      host_name: body.domain,
+      iso_week: iso_week
+    });
+
+    return response.send(true);
   }
 
   * create(request, response) {
