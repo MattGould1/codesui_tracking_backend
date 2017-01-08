0cd22a2dda8afd86dbe5fe2b935d76dbe2a48ab9
Matt <matthew.gould@dentaldepartures.com>
2017-01-08 11:02:16 +0000

    save user

--- app/Http/Controllers/ActivityController.js
+++ app/Http/Controllers/ActivityController.js
@@ -1,12 +1,34 @@
 'use strict'
 
+const User = use('App/Model/User')
+
 class ActivityController {
 
   /*
-    Create a user, update the activity_id and 
+    Create a user, update activity
+
+    request.all()
+    { 
+      session_id: 'b38bm9b5fo13114o363b109e956a96e2fo193',
+      email: 'wer@wrewer',
+      opportunity_type: '3',
+      activity_id: 'null',
+      purchase_id: 'default'
+    }
   */
   * create (request, response) {
-    console.log(request.all());
+    const body = request.all();
+    const user = yield User.query()
+                           .insert({
+                             email: body.email,
+                             session_id: body.session_id,
+                             location: null, //add this to the form later?
+                             age: null,
+                             gender: null,
+                             purchase_made: body.purchase_id,
+                             activity_id: body.activity_id
+                           });
+
     response.send(true);
   }
 
