78927158779b4cbcb392f6f71aa2e40ac38d0ef0
Matt <matthew.gould@dentaldepartures.com>
2017-01-09 08:24:16 +0000

    pretty

--- app/Model/Activity.js
+++ app/Model/Activity.js
@@ -3,9 +3,11 @@
 const Lucid = use('Lucid')
 
 class Activity extends Lucid {
+	
   session () {
     return this.belongsTo('App/Model/Session')
   }
+
   user () {
     return this.belongsTo('App/Model/User')
   }
