13d47af18922b54fe0a042867885a499a1c128fe
Matt <matthew.gould@dentaldepartures.com>
2017-01-18 16:03:48 +0000

    fix seeds for the database

--- database/seeds/Database.js
+++ database/seeds/Database.js
@@ -13,14 +13,66 @@
 */
 
 const Factory = use('Factory')
+const Fake = use('chance')
+
+const User = use('App/Model/User')
+const Activity = use('App/Model/Activity')
 
 class DatabaseSeeder {
 
   * run () {
-	//yield Factory.model('App/Model/User').create(300)
+    //reset
+    // yield Factory.model('App/Model/Session').reset()
+    // yield Factory.model('App/Model/User').reset()
+    // yield Factory.model('App/Model/Activity').reset()
 
-    //yield Factory.get('sessions').create(500)
-    yield Factory.get('activities').create(200)
+    var fake = new Fake()
+    const sessions = yield Factory.model('App/Model/Session').create(3)
+
+    sessions.each(function * (session) {
+      if (fake.bool() === true) {
+        var location = [
+          'thailand',
+          'england',
+          'usa',
+          'france'
+        ];
+
+        const user = new User()
+
+        user.email = fake.email()
+        user.session_id = session.session_id
+        user.location = fake.pickone(location)
+        user.age = fake.age()
+        user.gender = fake.gender()
+        user.purchase_made = fake.bool()
+
+        yield user.save()
+
+        var activity_id = [
+          'website',
+          'mobile'
+        ];
+
+        var opportunity_type = [
+          1,
+          2,
+          3,
+        ];
+
+        const activity = new Activity()
+
+        activity.activity_type = fake.pickone(opportunity_type)
+        activity.opportunity_type = fake.pickone(opportunity_type)
+        activity.session_id = session.session_id
+        activity.user_id = user.id
+        activity.activity_id = fake.pickone(activity_id)
+        activity.call_id = fake.fbid()
+
+        yield user.activity().save(activity)
+        process.exit()
+      }
+    })
   }
 
 }
