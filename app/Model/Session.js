1aa3df6c2f38171a705e9340a061a6f01f580968
Matt <matthew.gould@dentaldepartures.com>
2017-01-18 14:49:53 +0000

    add activity belong to relationship

--- app/Model/Session.js
+++ app/Model/Session.js
@@ -6,6 +6,10 @@ class Session extends Lucid {
   user () {
     return this.belongsTo('App/Model/User')
   }
+
+  activity() {
+  	return this.belongsto('App/Model/Activity')
+  }
 }
 
 module.exports = Session
