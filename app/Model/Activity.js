d8d63070dacb7fcfd5631ebd4ee3898b8c4df23d
Matt <matthew.gould@dentaldepartures.com>
2017-01-19 04:54:58 +0000

    activity relationship

--- app/Model/Activity.js
+++ app/Model/Activity.js
@@ -5,12 +5,12 @@ const Lucid = use('Lucid')
 class Activity extends Lucid {
 	
   session () {
-    return this.belongsToMany('App/Model/Session')
+    return this.belongsTo('App/Model/Session')
   }
 
-  user () {
-    return this.belongsToMany('App/Model/User')
-  }
+  // item () {
+  // 	return this.hasOne('App/Model/Item')
+  // }
 }
 
 module.exports = Activity
